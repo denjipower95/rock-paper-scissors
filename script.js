df58c0c2e3807673a366944b4a2a83aede558b9c
Charles Monsarvas <180669931+denjipower95@users.noreply.github.com>
2025-01-08 09:01:27 +0800

    Add function playRound

--- script.js
+++ script.js
@@ -25,3 +25,21 @@ function getHumanChoice() {
     getHumanChoice();
   }
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+
+  if (humanChoice === computerChoice) {
+    console.log(`It's a tie. Player and Computer both pick ${humanChoice}`);
+  } else if ( (humanChoice === 'rock' && computerChoice === 'paper') ||
+  (humanChoice === 'paper' && computerChoice === 'rock') ||
+  (humanChoice === 'scissors' && computerChoice === 'paper') ) {
+    console.log(`You won. ${humanChoice} beats ${computerChoice}`);
+    humanScore++;
+  } else {
+    console.log(`You lose. ${computerChoice} beats ${humanChoice}`);
+    computerScore++;
+  }
+}
